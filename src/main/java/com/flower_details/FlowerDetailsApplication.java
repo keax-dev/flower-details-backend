b9fcf92e3eff7a751fa6ef070567f280afe5e3f0
Kevin Galarza <jimenezkev1040@gmail.com>
2026-08-10 15:53:23 -0500

    feat: init project in spring boot

---
+++ src/main/java/com/flower_details/FlowerDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.flower_details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flower_details/FlowerDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flower_details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
